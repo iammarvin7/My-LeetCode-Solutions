d8cf2503ca7bc266252be01f57c4dfc07e1e0e39
Marvin Chaudhary <96001239+iammarvin7@users.noreply.github.com>
2025-04-21 17:22:24 -0500

    Time: 0 ms (100%), Space: 112.6 MB (47.66%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
+        long long mini = 0;
+        long long maxi = 0;
         long long sum = 0;
-        int n = differences.size();
-        for(int i = 0; i < n; ++i){
-            sum += differences[i];
-            if(sum > maxi) maxi = sum;
-            if(sum < mini) mini = sum;
+
+        for(int elem : differences){
+            sum += elem;
+            maxi = max(maxi, sum);
+            mini = min(mini, sum);
         }
-        int ans = 0;
-        for(int i = lower; i <= upper; ++i){
-            if((i+maxi) <= upper and (i+mini)>= lower) ans++;
+        //OLD CODE
+        // int ans = 0;
+        // for(int i = lower; i <= upper; ++i){
+        //     if((i+maxi) <= upper and (i+mini)>= lower) ans++;
             
-        }
-        return ans;
+        // }
+        // return ans;
+
+        //NEW CODE:- Instead of checking for each value, 
+        //check for how high the curve can go and still stay in the bount
+        /*
+        diff = [2, -4, 5];
+          x+2 
+          /\    x+1
+        x/  \  /
+             \/
+             x-2
+        if you start at (x-2) say p, how far up high can you go? (p + 4) will be the max value;
+        but if the upper bound is still 3 points above p, you can still have valid 3 sequences as, p+1, p+2, p+3;
+        therefore, the valid seqs will be (upper bound - range_of_diff) + 1 (make sure to include p too)
+        */
+        int ans = ((upper-lower) - (maxi-mini)) + 1;
+        return (ans > 0) ? ans : 0;
+
+
+
     }
 };
