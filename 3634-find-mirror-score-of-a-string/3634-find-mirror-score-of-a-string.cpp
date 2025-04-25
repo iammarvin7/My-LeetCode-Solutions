732a795db629fe3b05be5561fcd017351f22b76b
Marvin Chaudhary <96001239+iammarvin7@users.noreply.github.com>
2025-04-25 02:31:19 -0500

    Time: 23 ms (68.9%), Space: 20.5 MB (42.12%) - LeetHub

--- 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.cpp
+++ 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.cpp
@@ -8,22 +8,53 @@ public:
     long long calculateScore(string s) {
         long long score = 0;
         int n = s.size();
-        vector<vector<int>> vec(26, vector<int>());
+        unordered_map<char, stack<int>> past;
 
         for(int i = 0; i < n; ++i){
             char ch = s[i];
-            char m = mirror(ch); 
+            char m = mirror(ch);
 
-            if(vec[m-'a'].size() == 0){
-                vec[ch-'a'].push_back(i);
+            auto it = past.find(m);
+            if(it != past.end() and !it->second.empty()){
+                int v = it->second.top();
+                it->second.pop();
+                score = score + (i - v);
             }
-            else{
-                int v = vec[m-'a'].back();
-                score += (i - v);
-                vec[m-'a'].pop_back();
-            }   
+            else {
+                past[ch].push(i);
+            };
         }
         return score;
 
     }
 };
+
+// class Solution {
+
+//     char mirror(char ch){
+//         return 'z' - (ch - 'a');
+//     }
+
+// public:
+//     long long calculateScore(string s) {
+//         long long score = 0;
+//         int n = s.size();
+//         vector<vector<int>> vec(26, vector<int>());
+
+//         for(int i = 0; i < n; ++i){
+//             char ch = s[i];
+//             char m = mirror(ch); 
+
+//             if(vec[m-'a'].size() == 0){
+//                 vec[ch-'a'].push_back(i);
+//             }
+//             else{
+//                 int v = vec[m-'a'].back();
+//                 score += (i - v);
+//                 vec[m-'a'].pop_back();
+//             }   
+//         }
+//         return score;
+
+//     }
+// };
