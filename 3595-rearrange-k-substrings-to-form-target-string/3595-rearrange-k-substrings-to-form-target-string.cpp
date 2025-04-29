e717910e181c962e2e62f222a6ff1458188f9514
Marvin Chaudhary <96001239+iammarvin7@users.noreply.github.com>
2025-04-28 20:35:07 -0500

    Time: 260 ms (58.89%), Space: 84.5 MB (53.66%) - LeetHub

--- 3595-rearrange-k-substrings-to-form-target-string/3595-rearrange-k-substrings-to-form-target-string.cpp
+++ 3595-rearrange-k-substrings-to-form-target-string/3595-rearrange-k-substrings-to-form-target-string.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     bool isPossibleToRearrange(string s, string t, int k) {
-        unordered_map<string, int> t_freq;
+        unordered_map<string, int> freq;
         int n = s.size();
         int l = n/k;
-        int i = 0;
-        while(i < n){
-            string str = t.substr(i,l);
-            t_freq[str]++;
-            i+=l;
-        }
+        // int i = 0;
+        // while(i < n){
+        //     string str = t.substr(i,l);
+        //     t_freq[str]++;
+        //     i+=l;
+        // }
 
-        i = 0;
-        while(i < n){
-            string str = s.substr(i,l);
-            if(!(t_freq[str])){
-                return false;
-            }
-            else t_freq[str]--;
-            i += l;
-        }
+        // i = 0;
+        // while(i < n){
+        //     string str = s.substr(i,l);
+        //     if(!(t_freq[str])){
+        //         return false;
+        //     }
+        //     else t_freq[str]--;
+        //     i += l;
+        // }
+        // return true;
+
+        for(int i = 0; i < n; i+=l) freq[t.substr(i, l)]++;
+        for(int i = 0; i < n; i+=l) freq[s.substr(i, l)]--;
+
+        for(auto& [k, v] : freq) if(v) return false;
+
         return true;
+
     }
 };
